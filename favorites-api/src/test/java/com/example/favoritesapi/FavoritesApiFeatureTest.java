c66d178e007b6e3581ed47bfef1b06325587f321
Dtaitt <donovan.taitt@yahoo.com>
2018-03-25 11:51:16 -0400

    added test application.properties

--- favorites-api/src/test/java/com/example/favoritesapi/FavoritesApiFeatureTest.java
+++ favorites-api/src/test/java/com/example/favoritesapi/FavoritesApiFeatureTest.java
@@ -5,17 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class DemoApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class FavoritesApiFeatureTest {
+    @Autowired
+    private FavoriteRepository favoriteRepository;
 
+    @Before
+    public void setUp() {
+        favoriteRepository.deleteAll();
+    }
+
+    @After
+    public void tearDown() {
+        favoriteRepository.deleteAll();
+    }
 }
