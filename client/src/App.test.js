65e3cd05f26700e985827d80a68658258bfd1afb
Dtaitt <donovan.taitt@yahoo.com>
2018-04-17 18:28:12 -0400

    tested App

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import Header from './components/Header/Header';
+import MainContainer from './components/MainContainer';
+
+enzyme.configure({ adapter: new Adapter() });
+
+
+describe("App", function() {
+  it("should render <Header /> without throwing an error", function() {
+    expect(shallow(<App />).contains(<Header />)).toBe(
+      true
+    );
+  });
+
+  it("should render <MainContainer /> without throwing an error", function() {
+    expect(shallow(<App />).contains(<MainContainer />)).toBe(
+      true
+    );
+  });
 });
