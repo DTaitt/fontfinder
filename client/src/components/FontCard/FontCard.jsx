305894b5373d11f414faa9589ffbd42c3e980ae4
Dtaitt <donovan.taitt@yahoo.com>
2018-04-22 10:56:44 -0400

    moved logic to font card container

--- client/src/components/FontCard/FontCard.jsx
+++ client/src/components/FontCard/FontCard.jsx
@@ -26,175 +26,62 @@ type Props = {
 
 type State = {}
 
-export default class FontCard extends Component<Props, State>{
-
-    // componentDidMount() {
-    //     store.getState().favData.forEach(fav => {
-    //         if (fav.id === this.props.id) {
-    //             store.dispatch({
-    //                 type: 'UPDATE_FONT_DATA_FAV_STATUS',
-    //                 id: this.props.id,
-    //             }) 
-    //         }
-    //     })
-    // }
-
-    async addFavorite(fav:newFav) {
-        try {
-            await axios.post(`/favorites`, fav);
-        } catch (error) {
-            console.log(error);
-        }
-
-        //adds the favorite object if its not already added to favorites
-        const newFav:newFav = {
-            id: this.props.id,
-            family: this.props.family,
-            category: this.props.category,
-            url: this.props.url,
-        }
-
-        let found = store.getState().favData.find(fav => {
-          return fav.id === this.props.id;
-        });
-
-        // console.log(`Font Card fav status before click is ${this.props.isInFav.toString()}`)
-        // if(!found) {
-        //     store.dispatch({
-        //         type: "ADD_FAV_DATA",
-        //         new: newFav,
-        //     });
-    
-        //     store.dispatch({
-        //         type: 'UPDATE_FONT_DATA_FAV_STATUS',
-        //         id: this.props.id,
-        //     })  
-        // } else {
-        //     store.dispatch({
-        //         type: 'REMOVE_FAV_DATA',
-        //         favId: this.props.id,
-        //     })
-        //     store.dispatch({
-        //       type: "UPDATE_FONT_DATA_FAV_STATUS",
-        //       id: this.props.id
-        //     });
-        // }
-        // console.log(`Font Card fav status after click is ${this.props.isInFav.toString()}`);
-        if(!found) {
-            store.dispatch({
-                type: "ADD_FAV_DATA",
-                new: newFav,
-            });
-    
-            store.dispatch({
-                type: 'UPDATE_FONT_DATA_FAV_STATUS',
-                id: this.props.id,
-            })  
-        }
-    }
-
-    render() {
-        const favData:Object[] = store.getState().favData;
-        // let fontData = store.getState().fontData;
-        let isInFav:boolean = this.props.isInFav;
-
-        // console.log(store.getState().favData)
-        const newFav:newFav = {
-            id: this.props.id,
-            family: this.props.family,
-            category: this.props.category,
-            url: this.props.url,
-        }
-
-        const updatedFav:newFav = {
-            id: this.props.id,
-            family: this.props.family,
-            category: this.props.category,
-            url: this.props.url,
-        }
-
-        const variantOptions:Object[] = [
-            { value: "100", label: "Thin" },
-            { value: "100italic", label: "Thin Italic" },
-            { value: "200", label: "Extra-Light" },
-            { value: "200italic", label: "Extra-Light Italic" },
-            { value: "300", label: "Light" },
-            { value: "300italic", label: "Light Italic" },
-            { value: "regular", label: "Regular" },
-            { value: "italic", label: "Italic" },
-            { value: "500", label: "Medium" },
-            { value: "500italic", label: "Medium Italic" },
-            { value: "600", label: "Semi-Bold" },
-            { value: "600italic", label: "Semi-Bold Italic" },
-            { value: "700", label: "Bold" },
-            { value: "700italic", label: "Bold Italic" },
-            { value: "800", label: "Extra-Bold" },
-            { value: "800italic", label: "Extra-Bold Italic" },
-            { value: "900", label: "Black" },
-            { value: "900italic", label: "Black Italic" }
-        ];
-    
-        return(
-            <CardPanel 
-                className="font-card white black-text z-depth-2"
-            >
-                <div className="info">
-                    <a href={this.props.url} target='_blank'>
-                        <h1 className="family">{this.props.family}</h1>
-                    </a>
-                    <p className="category">{this.props.category}</p>
-                    <Dropdown trigger={
-                        <Button>{this.props.variants.length} Variant{this.props.variants.length > 1 && 's'}</Button>
-                    }>
-                    <Collection>
-                        {
-                            this.props.variants.map((variant) => {
-                                variantOptions.forEach((option) => {
-                                    if(variant === option.value) {
-                                        variant = option.label;
-                                    }
-                                })
-                                return <CollectionItem className='variant' key={variant}>{variant}</CollectionItem>
+export default function FontCard(props){
+    return(
+        <CardPanel 
+            className="font-card white black-text z-depth-2"
+        >
+            <div className="info">
+                <a href={props.url} target='_blank'>
+                    <h1 className="family">{props.family}</h1>
+                </a>
+                <p className="category">{props.category}</p>
+                <Dropdown trigger={
+                    <Button>{props.variants.length} Variant{props.variants.length > 1 && 's'}</Button>
+                }>
+                <Collection>
+                    {
+                        props.variants.map((variant) => {
+                            props.variantOptions.forEach((option) => {
+                                if(variant === option.value) {
+                                    variant = option.label;
+                                }
                             })
-                        }
-                    </Collection>
-                    </Dropdown>
-                </div>
-                <div className="interaction">
-                    <Button 
-                        floating 
-                        // small 
-                        className='red' 
-                        waves='light' 
-                        // icon={
-                        //     this.props.isInFav ? 'remove' : 'favorite'
-                        // } 
-                        icon='favorite'
-                        onClick={() => {
-                            // this.props.changeFavStatus()
-                            this.addFavorite(newFav)
-                        }}
-                    />
-                    <Modal
-                        header={this.props.family}
-                        trigger={<Button>Add Style</Button>}
-                        className='import-code'
-                    >
-                        <div className="html">
-                            <h2>Add to HTML</h2>
-                            <blockquote><pre><code>
-                                {`<link href="https://fonts.googleapis.com/css?family=${this.props.formattedFontFamily}" rel="stylesheet">`}
-                            </code></pre></blockquote>
-                        </div>
-                        <div className="css">
-                            <h2>Add to CSS</h2>
-                            <blockquote><pre><code>
-                                {`font-family: '${this.props.family}', ${this.props.category};`}
-                            </code></pre></blockquote>
-                        </div>                    
-                    </Modal>
-                </div>
-            </CardPanel>
-        )
-    }
+                            return <CollectionItem className='variant' key={variant}>{variant}</CollectionItem>
+                        })
+                    }
+                </Collection>
+                </Dropdown>
+            </div>
+            <div className="interaction">
+                <Button 
+                    floating 
+                    className='red' 
+                    waves='light' 
+                    icon='favorite'
+                    onClick={() => {
+                        props.addFavorite(props.newFav)
+                    }}
+                />
+                <Modal
+                    header={props.family}
+                    trigger={<Button>Add Style</Button>}
+                    className='import-code'
+                >
+                    <div className="html">
+                        <h2>Add to HTML</h2>
+                        <blockquote><pre><code>
+                            {`<link href="https://fonts.googleapis.com/css?family=${props.formattedFontFamily}" rel="stylesheet">`}
+                        </code></pre></blockquote>
+                    </div>
+                    <div className="css">
+                        <h2>Add to CSS</h2>
+                        <blockquote><pre><code>
+                            {`font-family: '${props.family}', ${props.category};`}
+                        </code></pre></blockquote>
+                    </div>                    
+                </Modal>
+            </div>
+        </CardPanel>
+    )
 }
--- client/src/components/FontCardContainer.jsx
+++ client/src/components/FontCardContainer.jsx
@@ -16,50 +16,99 @@ type Props = {
   isInFav: boolean,
 };
 
-export default class FontCardContainer extends Component {
+export default function FontCardContainer(props) {
 
-    formattedFontFamily: string = this.props.family;
-    favData = store.getState().favData;
-    //   console.log(favData)
-    found = store.getState().favData.find((fav) => {
-        return fav.id === this.props.id;
+    // FORMATTING FONT FAMILY IN MODAL
+    let formattedFontFamily: string = props.family;
+    const favData = store.getState().favData;
+    const found = store.getState().favData.find((fav) => {
+        return fav.id === props.id;
     })
-
-    componentDidMount() {
-        this.formatFontFamily();
+  
+    function formatFontFamily() {
+      let splitFontFamily = props.family.split(" ");
+      let joinedFontFamily = splitFontFamily.join("+");
+      formattedFontFamily = joinedFontFamily;
     }
     
-    componentDidUpdate(prevProps, prevState) {
-        // (this.found && this.props.isInFav === false)
-        // && 
-        // // console.log(this.props.isInFav)
-        // store.dispatch({
-        //   type: "UPDATE_FONT_DATA_FAV_STATUS",
-        //   id: this.props.id
-        // }); 
+    formatFontFamily()
+
+    // ADD FAVORITE FUNCTION
+    const newFav:newFav = {
+        id: props.id,
+        family: props.family,
+        category: props.category,
+        url: props.url,
     }
-  
-    formatFontFamily() {
-      let splitFontFamily = this.props.family.split(" ");
-      let joinedFontFamily = splitFontFamily.join("+");
-      this.formattedFontFamily = joinedFontFamily;
+
+    async function addFavorite(fav) {
+        try {
+            await axios.post(`/favorites`, fav);
+        } catch (error) {
+            console.log(error);
+        }
+
+        //adds the favorite object if its not already added to favorites
+        const newFav:newFav = {
+            id: props.id,
+            family: props.family,
+            category: props.category,
+            url: props.url,
+        }
+
+        let found = store.getState().favData.find(fav => {
+          return fav.id === props.id;
+        });
+
+        if(!found) {
+            store.dispatch({
+                type: "ADD_FAV_DATA",
+                new: newFav,
+            });
+    
+            store.dispatch({
+                type: 'UPDATE_FONT_DATA_FAV_STATUS',
+                id: props.id,
+            })  
+        }
     }
-        
-    render() {
-        // console.log(`Font Card Container fav status is ${this.props.isInFav.toString()}`)
-        // console.log(store.getState().favData)
-        return (
-             <Fragment>
-                <FontCard 
-                    id = {this.props.family}
-                    family = {this.props.family}
-                    category = {this.props.category}
-                    url = {`https://fonts.google.com/specimen/${this.props.family}`}
-                    variants = {this.props.variants}
-                    isInFav = {this.props.isInFav}
-                    formattedFontFamily = {this.formattedFontFamily}
-                />
-            </Fragment>
-        )
-    }
+
+    // FORMATTING VARIANT OPTIONS FOR DROPDOWN
+    const variantOptions:Object[] = [
+        { value: "100", label: "Thin" },
+        { value: "100italic", label: "Thin Italic" },
+        { value: "200", label: "Extra-Light" },
+        { value: "200italic", label: "Extra-Light Italic" },
+        { value: "300", label: "Light" },
+        { value: "300italic", label: "Light Italic" },
+        { value: "regular", label: "Regular" },
+        { value: "italic", label: "Italic" },
+        { value: "500", label: "Medium" },
+        { value: "500italic", label: "Medium Italic" },
+        { value: "600", label: "Semi-Bold" },
+        { value: "600italic", label: "Semi-Bold Italic" },
+        { value: "700", label: "Bold" },
+        { value: "700italic", label: "Bold Italic" },
+        { value: "800", label: "Extra-Bold" },
+        { value: "800italic", label: "Extra-Bold Italic" },
+        { value: "900", label: "Black" },
+        { value: "900italic", label: "Black Italic" }
+    ];
+
+    return (
+            <Fragment>
+            <FontCard 
+                id = {props.family}
+                family = {props.family}
+                category = {props.category}
+                url = {`https://fonts.google.com/specimen/${props.family}`}
+                variants = {props.variants}
+                isInFav = {props.isInFav}
+                formattedFontFamily = {formattedFontFamily}
+                addFavorite = {addFavorite}
+                newFav = {newFav}
+                variantOptions={variantOptions}
+            />
+        </Fragment>
+    )
 }
