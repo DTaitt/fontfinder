9c57a4017f28c5e396dd9b8f4aa144124c621680
Dtaitt <donovan.taitt@yahoo.com>
2018-04-23 08:52:24 -0400

    refactored Header test

--- client/src/components/Header/Header.test.js
+++ client/src/components/Header/Header.test.js
@@ -1,23 +1,21 @@
 import React from "react";
 import enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { Navbar } from "react-materialize";
+import { Navbar , Container} from "react-materialize";
 enzyme.configure({ adapter: new Adapter() });
 
 import Header from './Header';
 
 const HeaderWrapper = shallow(<Header />);
 
-const HeaderComponent = (
-  <header>
-    <div className="my-container">
-      <Navbar brand="Font Finder" />
-    </div>
-  </header>
-);
-
 describe("Header", function() {
   it("should render <Header /> without throwing an error", function() {
-    expect(HeaderWrapper.contains(HeaderComponent)).toBe(true);
+    expect(HeaderWrapper.exists(<header></header>)).toBe(true);
+  });
+  it("should render div with a class of 'my-container' without throwing an error", function() {
+    expect(HeaderWrapper.exists(<div className='my-container'></div>)).toBe(true);
+  });
+  it("should render <Navbar /> without throwing an error", function() {
+    expect(HeaderWrapper.exists(<Navbar brand='Font Finder' />)).toBe(true);
   });
 });
